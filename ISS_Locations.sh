da3e45cfc094c8c280066aff6133f2d8bb244534
TRGG3R <34950197+TRGG3R@users.noreply.github.com>
2018-03-31 16:48:01 -0400

    Update ISS_Locations.sh

--- ISS_Locations.sh
+++ ISS_Locations.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-/usr/bin/python /testnetload/ISS_loc1.py
-/usr/bin/python /testnetload/ISS_loc2.py
-/usr/bin/python /testnetload/ISS_loc3.py
-/usr/bin/python /testnetload/ISS_loc4.py
-/usr/bin/python /testnetload/ISS_loc5.py
+/usr/bin/python /home/ubuntu/testnetload/ISS_loc1.py
+/usr/bin/python /home/ubuntu/testnetload/ISS_loc2.py
+/usr/bin/python /home/ubuntu/testnetload/ISS_loc3.py
+/usr/bin/python /home/ubuntu/testnetload/ISS_loc4.py
+/usr/bin/python /home/ubuntu/testnetload/ISS_loc5.py
 
 
 
