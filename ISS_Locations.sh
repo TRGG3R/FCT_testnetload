f1c61fa3556fa7a91ca1ea00dccf46c707dde809
TRGG3R <34950197+TRGG3R@users.noreply.github.com>
2018-03-31 16:44:34 -0400

    Add files via upload

---
+++ ISS_Locations.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+/usr/bin/python /testnetload/ISS_loc1.py
+/usr/bin/python /testnetload/ISS_loc2.py
+/usr/bin/python /testnetload/ISS_loc3.py
+/usr/bin/python /testnetload/ISS_loc4.py
+/usr/bin/python /testnetload/ISS_loc5.py
+
+
+
+
